f3c55f0ccae7739fc6bdd91cc9023a4fdad8aba2
Koen Rademaker <24732704+kjradem@users.noreply.github.com>
2018-10-12 15:53:48 +0200

    added markup

--- scripts/classifier/classifier.R
+++ scripts/classifier/classifier.R
@@ -1,3 +1,6 @@
+#####################################################################
+
+# INSTALL AND LOACK PACKAGES
 packages <- c('gbm', 'naivebayes', 'tree', 'rpart', 'caret', 'e1071', 'rpart.plot')
 for(package in packages){
   if(is.element(package, installed.packages()[,1])==FALSE){
@@ -12,8 +15,10 @@ library(caret)
 library(e1071)
 library(rpart.plot)
 
-base_dir <- 'C://Users//daan//Documents//GitHub//RNA-Seq-analysis-tools'
-# Set base_dir to the specific folder where this repository is stored on your computer!
+# LOAD LOCATIONS
+
+base_dir <- ''
+  # Set base_dir to the specific folder where this repository is stored on your computer!
 script_dir <- paste(base_dir, '//scripts//classifier', sep='')
 data_dir <- paste(base_dir, '//example_data//', sep='')
 
@@ -24,17 +29,19 @@ source('classifier_functions.r')
 
 # LOAD DATAFRAMES
 
-file_list <- c("COAD.Rdata", "LUSC.Rdata", "THCA.Rdata")
+file_list <- c('COAD.Rdata', 'LUSC.Rdata', 'THCA.Rdata')
+  # Set file_list to a custom selection of files or to all using 'list.files(data_dir)' (not advised due to size)!
 
-cancer_types <- list("COAD","LUSC","THCA")
-labels <- list("Colorectal","Lung","Thyroid")
+# SET GLOBAL VARIABLES
+
+cancer_types <- list('COAD', 'LUSC', 'THCA')
+labels <- list('Colorectal', 'Lung', 'Thyroid')
 names(labels) <- cancer_types
 
 cancer_data <- data.frame()
 patient_data <- data.frame()
 MIN_N_PATIENTS = 10
 FOLD_CHANGE <- 0.05
-
 
 ####################################################################
 
@@ -56,21 +63,21 @@ patient_data <- t(cancer_data)
 
 patient_data_with_labels <- AppendLabels(patient_data, labels)
 
-
 ####################################################################
 
-
 # DEFINE PATIENT LABELS AS FACTOR
-y <- as.factor(patient_data_with_labels$label)
+patient_labels <- as.factor(patient_data_with_labels$label)
 
 # CREATE BASIC NAIVEBAYES MODEL WITHOUT TEST SET
-model.nb <- naiveBayes(patient_data, y)
-table(predict(model.nb, patient_data), y)
+model.nb <- naiveBayes(patient_data, patient_labels)
+table(predict(model.nb, patient_data), patient_labels)
 
 # cREATE NAIVE BAYES MODEL WITH CROSS VALIDATION
 
-model = train(x=patient_data,y=y,method='naive_bayes',trControl=trainControl(method='cv',number=10),metric="Accuracy")
+model = train(x=patient_data, y=patient_labels, method='naive_bayes', trControl=trainControl(method='cv',number=10), metric='Accuracy')
 
 # CREATE RPART DECISION TREE WITH CROSS VALIDATION
 
-model = train(x=patient_data,y=y,method='rpart',trControl=trainControl(method='cv',number=10),metric="Accuracy")
+model = train(x=patient_data, y=patient_labels, method='rpart', trControl=trainControl(method='cv',number=10), metric='Accuracy')
+
+####################################################################
